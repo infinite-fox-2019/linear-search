4e85ffc18b79291e149dff5f37482aa59c89c986
akbfitra <akbarfitralistyono@gmail.com>
2019-09-03 17:07:40 +0700

    linear akbfitra

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var arr =[]
+  for(var i = 0; i<values.length;i++){
+    if (target === values[i]){
+      arr.push(i)
+    }
+  }
+  if(!arr.length){ return -1}
+  else return arr[0]
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +25,12 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var out = []
+  for(var j = 0; j<values.length;j++){
+  if (target == values[j]){
+    out.push(j)
+    }
+  }return out
 }
 
 let banana_arr = "banana".split("");
