1238376dccbf513eb62454a84601c11995577178
anggabanny <anggabanny@gmail.com>
2019-09-03 11:51:44 +0700

    update

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,17 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  let count = 0
+  for(let i = 0 ; i < values.length; i++){
+    if (target == values[i]) {
+      count++
+    }
+  }
+  if (count == 0) {
+    return -1
+  } else {
+    return count
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +27,14 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  console.log(values);
+  let count = []
+  for(let i = 0 ; i < values.length; i++){
+    if (values[i] == target) {
+      count.push(i)
+    }
+  }
+  return count
 }
 
 let banana_arr = "banana".split("");
