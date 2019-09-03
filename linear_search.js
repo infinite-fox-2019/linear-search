046e0daae9e85c0ae407303188c463a5f38f8f4b
dipadana <putudipadana@gmail.com>
2019-09-03 13:36:07 +0700

    Kumpul linearSearch

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for(var i = 0; i < values.length; i++){
+    if(values[i] == target){
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,13 +21,20 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var counter = [];
+  for(var i = 0; i < values.length; i++){
+    if(values[i] == target){
+      counter.push(i);
+    }
+  }
+  return counter;
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
+console.log(globalLinearSearch("c", banana_arr));
 
 module.exports = {
   linearSearch,
