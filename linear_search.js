6a816c470a8ba8ee3a1e7dafd735ea6ebe0ef11e
Panji SN <panji1595@gmail.com>
2019-09-03 11:19:24 +0700

    tugas linear search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,13 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  var num = -1;
+  for(var i = values.length-1;i>=0;i--){
+    if(values[i]==target){
+      num = i;
+    }
+  }
+  return num;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +22,16 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var arr = [];
+  for(var i = 0;i<values.length;i++){
+    if(values[i]==target){
+      arr.push(i);
+    }
+  }
+  if (!arr){
+    return "Tidak ada input data";
+  }
+  return arr;
 }
 
 let banana_arr = "banana".split("");
