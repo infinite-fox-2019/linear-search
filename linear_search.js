340f9b59c39f6100fd0dd4fae12c06c5ace85527
rizkyich <ichsandirizky@gmail.com>
2019-09-04 06:58:07 +0700

    finish linear-search

--- linear_search.js
+++ linear_search.js
@@ -4,9 +4,13 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) return i
+  }
+  return -1
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -17,6 +21,11 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  const indexArr = []
+  for (let i = 0; i < values.length; i++) {
+    if(values[i] === target) indexArr.push(i)
+  }
+  return indexArr
 }
 
 let banana_arr = "banana".split("");
