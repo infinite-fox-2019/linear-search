2d8e99d5000bcf581750a1dae845b96fc9513f25
devitasari <devitas700@gmail.com>
2019-09-03 13:52:23 +0700

    add new file

--- linear_search.js
+++ linear_search.js
@@ -1,9 +1,15 @@
-'use strict'
+// 'use strict'
 
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (var i=0; i<values.length; i++) {
+    if (values[i] == target) {
+        return i
+    }
+  }
+return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +23,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var output = []
+  for (var i=0; i<banana_arr.length; i++) {
+    if (banana_arr[i] == target) {
+        output.push(i)
+    }
+  }
+  return output
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +37,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
