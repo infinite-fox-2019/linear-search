89b26b20cf3318ef825b55db765bfa7c69261bd2
anggabanny <anggabanny@gmail.com>
2019-09-03 13:38:20 +0700

    update

--- linear_search.js
+++ linear_search.js
@@ -4,17 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
-  let count = 0
   for(let i = 0 ; i < values.length; i++){
     if (target == values[i]) {
-      count++
+      return i
     }
   }
-  if (count == 0) {
-    return -1
-  } else {
-    return count
-  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -27,7 +22,7 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  console.log(values);
+  // console.log(values);
   let count = []
   for(let i = 0 ; i < values.length; i++){
     if (values[i] == target) {
