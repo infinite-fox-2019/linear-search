15b979c2985ae5ed743c498bb5b89cd3ab4c85db
aldinofrizal <aldinofrizal@gmail.com>
2019-09-03 13:41:49 +0700

    finish linear search

--- linear_search.js
+++ linear_search.js
@@ -4,19 +4,30 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(var i = 0; i < values.length; i++){
+    if(values[i] === target)return i
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, random_numbers));
-// 2
-console.log(linearSearch(9, random_numbers));
-// -1
+
+console.log(linearSearch(18, random_numbers));// 2
+console.log(linearSearch(9, random_numbers));// -1
+
+
+
 
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var result = []
+  for(var i = 0; i < values.length; i++){
+    if(values[i] === target)result.push(i)
+  }
+  return result
 }
 
 let banana_arr = "banana".split("");
